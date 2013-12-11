fc62bda9838dddc64aa897fccdb4f23ce4fda60d
Jonas Juselius <jonas.juselius@uit.no>
2013-12-11 11:24:53 +0100

    Fixed CMakeLists.txt.

--- compilers/FortranFlags.cmake
+++ compilers/FortranFlags.cmake
@@ -1,10 +1,10 @@
 if (NOT DEFINED DEFAULT_Fortran_FLAGS_SET)
 if(CMAKE_Fortran_COMPILER_ID MATCHES GNU) # this is gfortran
-    set(CMAKE_Fortran_FLAGS 
-        "-cpp -ffree-line-length-none -ff2c -fno-second-underscore")
-    set(CMAKE_Fortran_FLAGS_DEBUG   
+    set(CMAKE_Fortran_FLAGS
+        "-cpp -ffree-form -ffree-line-length-none -ff2c -fno-second-underscore")
+    set(CMAKE_Fortran_FLAGS_DEBUG
         "-O0 -g -fbacktrace")
-    set(CMAKE_Fortran_FLAGS_RELEASE 
+    set(CMAKE_Fortran_FLAGS_RELEASE
         "-w -O3 -ffast-math -funroll-loops -ftree-vectorize")
     if(ENABLE_BOUNDS_CHECK)
         set(CMAKE_Fortran_FLAGS
@@ -18,19 +18,19 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES GNU) # this is gfortran
     endif()
     add_definitions(-DHOST="gfortran" -DCACHE_SIZE=4096 -DMINLOOP=1)
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-    set(CMAKE_Fortran_FLAGS  
-        "-w -fpp -Wp,-f_com=no,-free -assume byterecl -traceback -lowercase")
+    set(CMAKE_Fortran_FLAGS
+        "-w -fpp -Wp,-f_com=no,-free -free -assume byterecl -traceback -lowercase")
     set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g")
     set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -xW -ip")
     if(ENABLE_BOUNDS_CHECK)
-        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} 
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}
         -check bounds -fpstkchk -check pointers -check uninit -check output_conversion"
             )
     endif()
     add_definitions(-DHOST="LinuxIFC" -DCACHE_SIZE=12000 -DPGF90 -Davoidalloc)
 
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES G95)
-    set(CMAKE_Fortran_FLAGS 
+    set(CMAKE_Fortran_FLAGS
         "-Wno=155 -fno-second-underscore -fsloppy-char")
     set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -ftrace=full")
     set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
